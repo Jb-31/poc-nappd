4df9bb385f0bd496afa34cfdcfe7da6b2c52917f
Jb-31 <jbrouanet@gmail.com>
2020-01-18 13:48:08 +0100

    updates

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 const { ApolloServer, gql } = require('apollo-server');
 const { prisma } = require('./generated/prisma-client')
 
+
 // Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
